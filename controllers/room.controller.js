c03c313de0f28d9640c2644f519593ddf84634c7
longshaww <at400123@gmail.com>
2021-10-14 03:12:34 +0700

    controller for room added

---
+++ controllers/room.controller.js
@@ -0,0 +1,74 @@
+const db = require("../db");
+const shortid = require("shortid");
+const { value, values } = require("../db");
+const { application } = require("express");
+
+module.exports.roomHome = (req, res) => {
+	res.render("rooms/roomhome", { rooms: db.get("rooms").value() });
+};
+module.exports.searchRoom = (req, res) => {
+	var q = req.query.q;
+	var matchedRooms = db
+		.get("rooms")
+		.value()
+		.filter(function (room) {
+			return (
+				room.room_type.toLowerCase().indexOf(q.toLowerCase()) !== -1
+			);
+		});
+	res.render("rooms/roomhome", {
+		rooms: matchedRooms,
+	});
+};
+
+module.exports.createRoomPug = (req, res) => {
+	res.render("rooms/createroom");
+};
+
+module.exports.createRoomValidation = (req, res) => {
+	req.body.id = shortid.generate();
+	var errors = [];
+	if (!req.body.room_id) {
+		errors.push("RoomID is required");
+	}
+	if (!req.body.room_type) {
+		errors.push("Room type is required");
+	}
+	if (!req.body.price) {
+		errors.push("Price is required");
+	}
+	if (!req.body.room_state) {
+		errors.push("Room state is required");
+	}
+	if (errors.length) {
+		res.render("rooms/createroom", {
+			errors: errors,
+			values: req.body,
+		});
+		return;
+	}
+	db.get("rooms").push(req.body).write();
+	res.redirect("/rooms");
+};
+module.exports.viewRoom = (req, res) => {
+	var id = req.params.id;
+	var room = db.get("rooms").find({ id: id }).value();
+	res.render("rooms/view", {
+		room: room,
+	});
+};
+module.exports.deleteRoom = (req, res) => {
+	var id = req.params.id;
+	db.get("rooms").remove({ id: id }).write();
+	res.redirect("/rooms");
+};
+module.exports.editRoomPug = (req, res) => {
+	var id = req.params.id;
+	var room = db.get("rooms").find({ id: id }).value();
+	res.render("rooms/edit", { room: room });
+};
+module.exports.editRoomHandling = (req, res) => {
+	var id = req.params.id;
+	db.get("rooms").find({ id: id }).assign(req.body).write();
+	res.redirect("/rooms/" + id);
+};
--- routes/rooms.route.js
+++ routes/rooms.route.js
@@ -1,78 +1,19 @@
 const express = require("express");
-const db = require("../db");
-const shortid = require("shortid");
-const { value, values } = require("../db");
-const { application } = require("express");
-// const controller = require("../controllers/room.controller");
+const controller = require("../controllers/room.controller");
 const router = express.Router();
 
-router.get("/", (req, res) => {
-	res.render("rooms/roomhome", { rooms: db.get("rooms").value() });
-});
-router.get("/search", (req, res) => {
-	var q = req.query.q;
-	var matchedRooms = db
-		.get("rooms")
-		.value()
-		.filter(function (room) {
-			return (
-				room.room_type.toLowerCase().indexOf(q.toLowerCase()) !== -1
-			);
-		});
-	res.render("rooms/roomhome", {
-		rooms: matchedRooms,
-	});
-});
-router.get("/create", (req, res) => {
-	res.render("rooms/createroom");
-});
-router.post("/create", (req, res) => {
-	req.body.id = shortid.generate();
-	var errors = [];
-	if (!req.body.room_id) {
-		errors.push("RoomID is required");
-	}
-	if (!req.body.room_type) {
-		errors.push("Room type is required");
-	}
-	if (!req.body.price) {
-		errors.push("Price is required");
-	}
-	if (!req.body.room_state) {
-		errors.push("Room state is required");
-	}
-	if (errors.length) {
-		res.render("rooms/createroom", {
-			errors: errors,
-			values: req.body,
-		});
-		return;
-	}
-	db.get("rooms").push(req.body).write();
-	res.redirect("/rooms");
-});
+router.get("/", controller.roomHome);
 
-router.get("/:id", (req, res) => {
-	var id = req.params.id;
-	var room = db.get("rooms").find({ id: id }).value();
-	res.render("rooms/view", {
-		room: room,
-	});
-});
-router.post("/delete/:id", (req, res) => {
-	var id = req.params.id;
-	db.get("rooms").remove({ id: id }).write();
-	res.redirect("/rooms");
-});
-router.get("/:id/edit", (req, res) => {
-	var id = req.params.id;
-	var room = db.get("rooms").find({ id: id }).value();
-	res.render("rooms/edit", { room: room });
-});
-router.post("/:id/edit", (req, res) => {
-	var id = req.params.id;
-	db.get("rooms").find({ id: id }).assign(req.body).write();
-	res.redirect("/rooms/" + id);
-});
+router.get("/search", controller.searchRoom);
+
+router.get("/create", controller.createRoomPug);
+router.post("/create", controller.createRoomValidation);
+
+router.get("/:id", controller.viewRoom);
+router.post("/delete/:id", controller.deleteRoom);
+
+router.get("/:id/edit", controller.editRoomPug);
+
+router.post("/:id/edit", controller.editRoomHandling);
 
 module.exports = router;
