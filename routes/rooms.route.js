343474a64a64f238e86a87cc4c0e4bc3d18d9f18
longshaww <at400123@gmail.com>
2021-10-13 14:54:43 +0700

    update Rooms

--- routes/rooms.route.js
+++ routes/rooms.route.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const db = require("../db");
+const shortid = require("shortid");
+const { value, values } = require("../db");
+const { application } = require("express");
 // const controller = require("../controllers/room.controller");
 const router = express.Router();
 
@@ -19,5 +22,46 @@ router.get("/search", (req, res) => {
 	res.render("rooms/roomhome", {
 		rooms: matchedRooms,
 	});
+});
+router.get("/create", (req, res) => {
+	res.render("rooms/createroom");
+});
+router.post("/create", (req, res) => {
+	req.body.id = shortid.generate();
+	var errors = [];
+	if (!req.body.room_id) {
+		errors.push("RoomID is required");
+	}
+	if (!req.body.room_type) {
+		errors.push("Room type is required");
+	}
+	if (!req.body.price) {
+		errors.push("Price is required");
+	}
+	if (!req.body.room_state) {
+		errors.push("Room state is required");
+	}
+	if (errors.length) {
+		res.render("rooms/createroom", {
+			errors: errors,
+			values: req.body,
+		});
+		return;
+	}
+	db.get("rooms").push(req.body).write();
+	res.redirect("/rooms");
+});
+
+router.get("/:id", (req, res) => {
+	var id = req.params.id;
+	var room = db.get("rooms").find({ id: id }).value();
+	res.render("rooms/view", {
+		room: room,
+	});
+});
+router.delete("/:id", (req, res) => {
+	var id = req.params.id;
+	var delRoom = db.get("rooms").remove({ id: id }).write();
+	res.send(delRoom);
 });
 module.exports = router;
